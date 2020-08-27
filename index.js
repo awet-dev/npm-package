a991a4a6b6e305a7c95832f5a44c77ed4c46b7b8
Awet Tesfay <whitezscope@gmail.com>
2020-08-27 16:24:01 +0200

    add file

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 
 // onclick display the source of js cheat sheet
 
-const userInput = document.querySelector(".form-control").value;
-document.querySelector("#btn").addEventListener("click", ()=> {
-   alert(userInput);
-})
+const userInput = document.querySelector("#first-name").value;
+if (userInput) {
+   document.querySelector("#btn").addEventListener("click", ()=> {
+      alert(userInput);
+   })
+}
+(function() {
+   // to get the value of an input: document.getElementById("element-id").value
+   let v1 = parseFloat(document.getElementById("op-one").value);
+   let v2 = parseFloat(document.getElementById("op-two").value);
+
+   const performOperation = function(operation) {
+      // perform the operation
+      switch (operation) {
+         case "addition":
+            alert(v1 + v2);
+            break;
+         case "subtraction":
+            alert(v1 - v2);
+            break;
+         case "multiplication":
+            alert(v1 * v2);
+            break;
+         case "division":
+            alert(v1 / v2);
+            break;
+      }
+   };
+
+   Array.from(document.querySelectorAll("button.operator")).forEach(function($btn) {
+      $btn.addEventListener("click", function() {
+         performOperation($btn.id);
+      });
+   });
+})();
